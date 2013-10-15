f46618550b55a65690affaccb55703f9f87f37c8
Dmitry Ananichev <a@qozz.ru>
2013-10-15 16:30:22 +0600

    Configuration class init test + basic folder structure for test + keywords fix in composer.json

---
+++ test/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+if  (
+    !($loader = @include __DIR__ . '/../vendor/autoload.php')
+    && !($loader = @include __DIR__ . '/../../../autoload.php')
+) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+/* @var $loader \Composer\Autoload\ClassLoader */
+$loader->add('Steam\\', __DIR__);
---
+++ test/Steam/Configuration_Test.php
@@ -0,0 +1,58 @@
+<?php
+namespace Steam;
+namespace Steam\Exception;
+namespace PHPUnit;
+use PHPUnit_Framework_TestCase;
+use Steam\Configuration;
+use Steam\Exception\InvalidConfigOptionException;
+
+require __DIR__ . '/../../src/Steam/Configuration.php';
+require __DIR__ . '/../../src/Steam/Exception/InvalidConfigOptionException.php';
+
+class Configuration_Test extends PHPUnit_Framework_TestCase {
+
+    public function test_setSteamKey()
+    {
+        $steamKey = '123';
+        $configuration = new Configuration();
+        $this->assertInstanceOf('\Steam\Configuration', $configuration->setSteamKey($steamKey));
+    }
+
+    public function test_getSteamKey()
+    {
+        $steamKey = '123';
+        $configuration = new Configuration();
+        $configuration->setSteamKey($steamKey);
+        $this->assertEquals($steamKey, $configuration->getSteamKey());
+    }
+
+    public function test_setAppId()
+    {
+        $appId = 123;
+        $configuration = new Configuration();
+        $this->assertInstanceOf('\Steam\Configuration', $configuration->setAppId($appId));
+    }
+
+    public function test_getAppId()
+    {
+        $appId = 123;
+        $configuration = new Configuration();
+        $configuration->setAppId($appId);
+        $this->assertEquals($appId, $configuration->getAppId());
+    }
+
+    public function test_getBaseSteamApiUrl()
+    {
+        $steamBaseUrl = 'http://api.steampowered.com/';
+        $configuration = new Configuration();
+        $this->assertEquals($steamBaseUrl, $configuration->getBaseSteamApiUrl());
+    }
+
+    public function test_setOptions_Exception()
+    {
+        $this->setExpectedException('\Steam\Exception\InvalidConfigOptionException');
+        $options = array('soft-kitty-warm-kitty-little-ball-of-fur' => true);
+        $configuration = new Configuration();
+        $configuration->setOptions($options);
+    }
+}
