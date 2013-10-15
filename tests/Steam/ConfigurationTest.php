e70b8c6218248bac590b3762a3189141ea01d7fa
Dan Mitchell <danmitchell@ntlworld.com>
2013-10-15 18:26:54 +0100

    Renamed test folder to tests and did some minor refactoring on the unit testing bootstrapping

--- tests/Steam/ConfigurationTest.php
+++ tests/Steam/ConfigurationTest.php
@@ -1,24 +1,18 @@
 <?php
 namespace Steam;
-namespace Steam\Exception;
-namespace PHPUnit;
-use PHPUnit_Framework_TestCase;
+
 use Steam\Configuration;
-use Steam\Exception\InvalidConfigOptionException;
 
-require __DIR__ . '/../../src/Steam/Configuration.php';
-require __DIR__ . '/../../src/Steam/Exception/InvalidConfigOptionException.php';
-
-class ConfigurationTest extends PHPUnit_Framework_TestCase {
-
-    public function test_setSteamKey()
+class ConfigurationTest extends \PHPUnit_Framework_TestCase 
+{
+    public function testSetSteamKey()
     {
         $steamKey = '123';
         $configuration = new Configuration();
         $this->assertInstanceOf('\Steam\Configuration', $configuration->setSteamKey($steamKey));
     }
 
-    public function test_getSteamKey()
+    public function testGetSteamKey()
     {
         $steamKey = '123';
         $configuration = new Configuration();
@@ -26,14 +20,14 @@ class ConfigurationTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($steamKey, $configuration->getSteamKey());
     }
 
-    public function test_setAppId()
+    public function testSetAppId()
     {
         $appId = 123;
         $configuration = new Configuration();
         $this->assertInstanceOf('\Steam\Configuration', $configuration->setAppId($appId));
     }
 
-    public function test_getAppId()
+    public function testGetAppId()
     {
         $appId = 123;
         $configuration = new Configuration();
@@ -41,14 +35,14 @@ class ConfigurationTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($appId, $configuration->getAppId());
     }
 
-    public function test_getBaseSteamApiUrl()
+    public function testGetBaseSteamApiUrl()
     {
         $steamBaseUrl = 'http://api.steampowered.com/';
         $configuration = new Configuration();
         $this->assertEquals($steamBaseUrl, $configuration->getBaseSteamApiUrl());
     }
 
-    public function test_setOptions_Exception()
+    public function testSetOptions_Exception()
     {
         $this->setExpectedException('\Steam\Exception\InvalidConfigOptionException');
         $options = array('soft-kitty-warm-kitty-little-ball-of-fur' => true);
