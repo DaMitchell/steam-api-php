ea358e4b846443be38740e631bc564f218213fbf
Dmitry Ananichev <a@qozz.ru>
2013-10-22 20:34:33 +0600

    cs-fixer removed; user api + tests

--- src/Steam/Api/User.php
+++ src/Steam/Api/User.php
@@ -2,7 +2,7 @@
 
 namespace Steam\Api;
 
-use Steam\Api\Exception\ApiNotImplementedException;
+use Steam\Api\Exception\InsufficientParameters;
 use Steam\Api\Exception\UserNotExists;
 use Steam\Steam;
 
@@ -16,7 +16,7 @@ class User extends Steam
     /**
      * @link https://developer.valvesoftware.com/wiki/Steam_Web_API#GetFriendList_.28v0001.29
      *
-     * @param int $steamid
+     * @param int    $steamid
      * @param string $relationship
      *
      * @throws Exception\UserNotExists
@@ -46,19 +46,114 @@ class User extends Steam
         return $this->getAdapter()->request($url, $requestParams)->getParsedBody();
     }
 
-    public function getPlayerBans()
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetPlayerBans
+     *
+     * @param array $steamids
+     *
+     * @throws Exception\UserNotExists
+     * @throws Exception\InsufficientParameters
+     *
+     * @return array
+     */
+    public function getPlayerBans($steamids = array(), $dump = false)
     {
-        throw new ApiNotImplementedException(sprintf('"%s" has not been implemented', __METHOD__));
+        $notExistsUsers = array();
+        $url = self::ENDPOINT_BASE . 'GetPlayerBans/v1/';
+
+        if (count($steamids) > 0) {
+            foreach ($steamids as $key=>$id) {
+                if (!is_numeric($id)) {
+                    $resolvedUrl = $this->resolveVanityUrl($id);
+                    if ($resolvedUrl['response']['success'] == 1) {
+                        $steamids[$key] = $resolvedUrl['response']['steamid'];
+                    } else {
+                        $notExistsUsers[] = $id;
+                    }
+                }
+            }
+        } else {
+            throw new InsufficientParameters('You need to pass at least one steam id');
+        }
+
+        if (count($notExistsUsers) > 0) {
+            throw new UserNotExists(sprintf('User(-s) with url(-s) "%s" not found', join(', ', $notExistsUsers)));
+        }
+
+        $requestParams = array(
+            'steamids' => join(',', $steamids)
+        );
+
+        return $this->getAdapter()->request($url, $requestParams)->getParsedBody();
     }
 
-    public function getPlayerSummaries()
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetPlayerBans
+     *
+     * @param array $steamids
+     *
+     * @throws Exception\UserNotExists
+     * @throws Exception\InsufficientParameters
+     *
+     * @return array
+     */
+    public function getPlayerSummaries($steamids)
     {
-        throw new ApiNotImplementedException(sprintf('"%s" has not been implemented', __METHOD__));
+        $notExistsUsers = array();
+        $url = self::ENDPOINT_BASE . 'GetPlayerSummaries/v0002/';
+
+        if (count($steamids) > 0) {
+            foreach ($steamids as $key=>$id) {
+                if (!is_numeric($id)) {
+                    $resolvedUrl = $this->resolveVanityUrl($id);
+                    if ($resolvedUrl['response']['success'] == 1) {
+                        $steamids[$key] = $resolvedUrl['response']['steamid'];
+                    } else {
+                        $notExistsUsers[] = $id;
+                    }
+                }
+            }
+        } else {
+            throw new InsufficientParameters('You need to pass at least one steam id');
+        }
+
+        if (count($notExistsUsers) > 0) {
+            throw new UserNotExists(sprintf('User(-s) with url(-s) "%s" not found', join(', ', $notExistsUsers)));
+        }
+
+        $requestParams = array(
+            'steamids' => join(',', $steamids)
+        );
+
+        return $this->getAdapter()->request($url, $requestParams)->getParsedBody();
     }
 
-    public function getUserGroupList()
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetUserGroupList
+     *
+     * @param int $steamid
+     *
+     * @throws Exception\UserNotExists
+     * @return array
+     */
+    public function getUserGroupList($steamid)
     {
-        throw new ApiNotImplementedException(sprintf('"%s" has not been implemented', __METHOD__));
+        $url = self::ENDPOINT_BASE . 'GetUserGroupList/v1/';
+
+        if (!is_numeric($steamid)) {
+            $resolvedUrl = $this->resolveVanityUrl($steamid);
+            if ($resolvedUrl['response']['success'] == 1) {
+                $steamid = $resolvedUrl['response']['steamid'];
+            } else {
+                throw new UserNotExists(sprintf('User with url "%s" does not exists', $steamid));
+            }
+        }
+
+        $requestParams = array(
+            'steamid' => $steamid
+        );
+
+        return $this->getAdapter()->request($url, $requestParams)->getParsedBody();
     }
 
     /**
@@ -78,4 +173,5 @@ class User extends Steam
             ))
             ->getParsedBody();
     }
+
 }
