3b756ee26e2c7e92de198bb2862059c215be86f7
Dan Mitchell <danmitchell@ntlworld.com>
2013-10-31 07:30:21 +0000

    a start on the dota match wrapper

--- src/Steam/Api/Dota2.php
+++ src/Steam/Api/Dota2.php
@@ -1,10 +1,77 @@
 <?php
 
 namespace Steam\Api;
- 
+
+use Steam\Api\Exception\InsufficientParameters;
 use Steam\Steam;
 
 class Dota2 extends Steam
 {
-    
+    const ENDPOINT_BASE = 'IEconDOTA2_{id}/';
+
+    /**
+     * @var string
+     */
+    protected $_endpoint = null;
+
+    /**
+     * @return string
+     * @throws InsufficientParameters
+     */
+    protected function getEndPoint()
+    {
+        if(is_null($this->_endpoint))
+        {
+            $appId = $this->getAdapter()->getConfig()->getAppId();
+
+            if(empty($appId))
+            {
+                throw new InsufficientParameters('You need to set a appId in the config to use this method');
+            }
+
+            $this->_endpoint = str_replace(self::ENDPOINT_BASE, '{id}', $appId);
+        }
+
+        return $this->_endpoint;
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetRarities
+     *
+     * @return array
+     */
+    public function getRarities()
+    {
+        $url = $this->getEndPoint() . 'GetRarities/v1';
+
+        return $this->getAdapter()->request($url)->getParsedBody();
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetHeroes
+     *
+     * @param bool $itemizedOnly
+     *
+     * @return array
+     */
+    public function getHeroes($itemizedOnly)
+    {
+        $params = array(
+            'itemizedonly' => (bool) $itemizedOnly
+        );
+
+        $url = $this->getEndPoint() . 'GetHeroes/v1';
+
+        return $this->getAdapter()->request($url, $params)->getParsedBody();
+    }
+
+    /**
+     * @return array
+     */
+    public function getTournamentPrizePool()
+    {
+        $url = $this->getEndPoint() . 'GetTournamentPrizePool/v1';
+
+        return $this->getAdapter()->request($url)->getParsedBody();
+    }
 }
--- src/Steam/Api/Dota2/Match.php
+++ src/Steam/Api/Dota2/Match.php
@@ -2,7 +2,136 @@
 
 namespace Steam\Api\Dota2;
  
-class Match 
+use Steam\Api\Exception\InsufficientParameters;
+use Steam\Steam;
+
+class Match extends Steam
 {
-    
+    const ENDPOINT_BASE = 'IDOTA2Match_{id}/';
+
+    /**
+     * @var string
+     */
+    protected $_endpoint = null;
+
+    /**
+     * @return string
+     * @throws InsufficientParameters
+     */
+    protected function getEndPoint()
+    {
+        if(is_null($this->_endpoint))
+        {
+            $appId = $this->getAdapter()->getConfig()->getAppId();
+
+            if(empty($appId))
+            {
+                throw new InsufficientParameters('You need to set a appId in the config to use this method');
+            }
+
+            $this->_endpoint = str_replace(self::ENDPOINT_BASE, '{id}', $appId);
+        }
+
+        return $this->_endpoint;
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetLeagueListing
+     *
+     * @return array
+     */
+    public function getLeagueListing()
+    {
+        $url = $this->getEndPoint() . 'GetLeagueListing/v1';
+
+        return $this->getAdapter()->request($url)->getParsedBody();
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetLiveLeagueGames
+     *
+     * @return array
+     */
+    public function getLiveLeagueGames()
+    {
+        $url = $this->getEndPoint() . 'GetLiveLeagueGames/v1';
+
+        return $this->getAdapter()->request($url)->getParsedBody();
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetMatchDetails
+     *
+     * @param int $matchId
+     *
+     * @return array
+     */
+    public function getMatchDetails($matchId)
+    {
+        $params = array(
+            'match_id' => $matchId
+        );
+
+        $url = $this->getEndPoint() . 'GetMatchDetails/v1';
+
+        $this->getAdapter()->request($url, $params)->getParsedBody();
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetMatchHistory
+     *
+     * @return array
+     */
+    public function getMatchHistory()
+    {
+        $url = $this->getEndPoint() . 'GetMatchHistory/v1';
+
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetMatchHistoryBySequenceNum
+     *
+     * @param int $startAtMatchSeqNumber
+     * @param $matchesRequested
+     *
+     * @return array
+     */
+    public function getMatchHistoryBySequenceNum($startAtMatchSeqNumber, $matchesRequested)
+    {
+        $params = array(
+            'start_at_match_seq_num' => $startAtMatchSeqNumber,
+            'matches_requested' => $matchesRequested
+        );
+
+        $url = $this->getEndPoint() . 'GetMatchHistoryBySequenceNum/v1';
+
+        $this->getAdapter()->request($url, $params)->getParsedBody();
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetScheduledLeagueGames
+     */
+    public function getScheduledLeagueGames()
+    {
+        $url = $this->getEndPoint() . 'GetScheduledLeagueGames/v1';
+
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetTeamInfoByTeamID
+     */
+    public function getTeamInfoByTeamId()
+    {
+        $url = $this->getEndPoint() . 'GetTeamInfoByTeamID/v1';
+
+    }
+
+    /**
+     * @link http://wiki.teamfortress.com/wiki/WebAPI/GetTournamentPlayerStats
+     */
+    public function GetTournamentPlayerStats()
+    {
+        $url = $this->getEndPoint() . 'GetTournamentPlayerStats/v1';
+
+    }
 }
