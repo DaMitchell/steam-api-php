0027d8fbedb35b833fc27e9d96052b4f1bb8a657
Dan Mitchell <danmitchell@ntlworld.com>
2013-11-04 13:51:55 +0000

    tests for the portal 2 leaderboards and made the AdapterAbstract an actual abstract class

--- src/Steam/Adapter/AdapterAbstract.php
+++ src/Steam/Adapter/AdapterAbstract.php
@@ -1,18 +1,11 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Dan
- * Date: 27/06/13
- * Time: 15:08
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Steam\Adapter;
 
 use JMS\Serializer\Serializer;
 use Steam\Configuration;
 
-class AdapterAbstract
+abstract class AdapterAbstract
 {
     /**
      * @var Configuration
