c732c49316eea56ac347c47fdd0ca2e0a9092e02
Dan Mitchell <danmitchell@ntlworld.com>
2015-05-18 18:04:48 +0100

    more commands and tests

---
+++ src/Steam/Command/Dota2/Match/GetTournamentPlayerStats.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Steam\Command\Dota2\Match;
+
+use Steam\Traits\Dota2CommandTrait;
+
+class GetTournamentPlayerStats
+{
+    use Dota2CommandTrait;
+
+    /**
+     * @var string
+     */
+    protected $accountId;
+
+    /**
+     * @var string
+     */
+    protected $leagueId;
+
+    /**
+     * @var string
+     */
+    protected $heroId;
+
+    /**
+     * @var int
+     */
+    protected $matchId;
+
+    /**
+     * @param string $accountId
+     */
+    public function __construct($accountId)
+    {
+        $this->accountId = $accountId;
+    }
+
+    /**
+     * @param string $leagueId
+     *
+     * @return self
+     */
+    public function setLeagueId($leagueId)
+    {
+        $this->leagueId = $leagueId;
+        return $this;
+    }
+
+    /**
+     * @param string $heroId
+     *
+     * @return self
+     */
+    public function setHeroId($heroId)
+    {
+        $this->heroId = $heroId;
+        return $this;
+    }
+
+    /**
+     * @param int $matchId
+     *
+     * @return self
+     */
+    public function setMatchId($matchId)
+    {
+        $this->matchId = $matchId;
+        return $this;
+    }
+
+    public function getInterface()
+    {
+        return 'IDOTA2Match_' . $this->getDota2AppId();
+    }
+
+    public function getMethod()
+    {
+        return 'GetTournamentPlayerStats';
+    }
+
+    public function getVersion()
+    {
+        return 'v1';
+    }
+
+    public function getRequestMethod()
+    {
+        return 'GET';
+    }
+
+    public function getParams()
+    {
+        $params = [
+            'account_id' => $this->accountId,
+        ];
+
+        empty($this->leagueId) ?: $params['league_id'] = $this->leagueId;
+        empty($this->heroId) ?: $params['hero_id'] = $this->heroId;
+        empty($this->matchId) ?: $params['match_id'] = $this->matchId;
+
+        return $params;
+    }
+} 
---
+++ tests/src/Steam/Command/Dota2/Match/GetTournamentPlayerStatsTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Steam\Command\Dota2\Match;
+
+class GetTournamentPlayerStatsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var GetTournamentPlayerStats
+     */
+    protected $instance;
+
+    public function setUp()
+    {
+        $this->instance = new GetTournamentPlayerStats(123);
+    }
+
+    public function testValues()
+    {
+        $this->assertEquals('GetTournamentPlayerStats', $this->instance->getMethod());
+        $this->assertEquals('v1', $this->instance->getVersion());
+        $this->assertEquals('GET', $this->instance->getRequestMethod());
+        $this->assertEquals(['account_id' => 123], $this->instance->getParams());
+    }
+
+    public function testInterfaceForDota2TestClient()
+    {
+        $this->instance->isForTestClient(true);
+        $this->assertEquals('IDOTA2Match_205790', $this->instance->getInterface());
+    }
+
+    public function testInterfaceForDota2Client()
+    {
+        $this->instance->isForTestClient(false);
+        $this->assertEquals('IDOTA2Match_570', $this->instance->getInterface());
+    }
+
+    public function testSettingLeague()
+    {
+        $this->instance->setLeagueId(123);
+
+        $this->assertEquals(['account_id' => 123, 'league_id' => 123], $this->instance->getParams());
+    }
+
+    public function testSettingMatchId()
+    {
+        $this->instance->setMatchId(123);
+
+        $this->assertEquals(['account_id' => 123, 'match_id' => 123], $this->instance->getParams());
+    }
+
+    public function testSettingHeroId()
+    {
+        $this->instance->setHeroId(123);
+
+        $this->assertEquals(['account_id' => 123, 'hero_id' => 123], $this->instance->getParams());
+    }
+
+    public function testSettingDateMinAndMax()
+    {
+        $this->instance->setLeagueId(123);
+        $this->instance->setHeroId(456);
+        $this->instance->setMatchId(789);
+
+        $this->assertEquals([
+            'account_id' => 123,
+            'league_id' => 123,
+            'hero_id' => 456,
+            'match_id' => 789,
+        ], $this->instance->getParams());
+    }
+}
+ 
